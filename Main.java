aadc3397da2b218fc4e7dbedbec50070406f49c3
Treyway1 <treyvor.romero-jones@patelhs.org>
2021-01-29 17:26:12 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    
+    Scanner scan = new Scanner(System.in);
+    
+    System.out.println("What is your favorite candy?");
+    
+    String name = new scan.next();
+  }
+}
